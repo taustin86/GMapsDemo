25e0027f8171ed40596c8f2e66ee13d9d92603fc
Tamara Austin <tamara.austin08@gmail.com>
2015-08-10 05:04:00 -0400

    Initial media viewer for map stories

---
+++ GMapsDemo/MediaViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MediaViewController.swift
+//  GMapsDemo
+//
+//  Created by Tamara Austin on 8/7/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class MediaViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title:"Map", style:.Plain, target:self, action:Selector("backToMapTapped"))
+        self.navigationItem.hidesBackButton = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func circleTapped(sender:UIButton) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func backToMapTapped() {
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
